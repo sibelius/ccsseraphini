e3aa111669c9b71db4f3c47de88ad57261e9a6c4
Vitor <vi.souza.almeida@protonmail.com>
2022-01-14 09:32:44 -0300

    feat: github link to the repo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Box, Flex, Button, Textarea, Badge, Image } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {
+  Box,
+  Flex,
+  Button,
+  Textarea,
+  Badge,
+  Image,
+  Link,
+} from '@chakra-ui/react';
 import { FaTwitter } from 'react-icons/fa';
 import { useState } from 'react';
 
@@ -16,6 +25,12 @@ const Home: NextPage = () => {
         <title>cc @sseraphini</title>
         <meta name="description" content="Make it easy to cc @sseraphini" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+          integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
+          crossOrigin="anonymous"
+        ></link>
       </Head>
 
       <Flex
@@ -58,6 +73,9 @@ const Home: NextPage = () => {
             {counter}
           </Badge>
           <Box as="span">cc @sseraphini</Box>
+          <NextLink href={''}>
+            <Link href="https://github.com/sibelius/ccsseraphini">Github</Link>
+          </NextLink>
 
           <Button
             colorScheme="twitter"
