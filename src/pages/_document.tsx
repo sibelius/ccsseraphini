08aadd1b0276ec402be83343d0818730fd6af0ee
Antonio Anderson <hiroto.anderson@gmail.com>
2022-01-14 14:02:51 -0300

    removing class

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,20 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 import theme from '../theme/theme';
 
-export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <div>
-            <title>cc @sseraphini</title>
-            <meta name="description" content="Make it easy to cc @sseraphini" />
-            <link rel="icon" href="/favicon.ico" />
-          </div>
-        </Head>
-
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <title>cc @sseraphini</title>
+      <Head>
+        <meta name="description" content="Make it easy to cc @sseraphini" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
