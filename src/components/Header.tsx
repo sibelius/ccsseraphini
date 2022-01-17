d8584bcae277a07ae7ff0c89a45f55e69744da68
Antonio Anderson <hiroto.anderson@gmail.com>
2022-01-17 09:20:05 -0300

    Update src/components/Header.tsx
    Co-authored-by: wesleynepo <50495587+wesleynepo@users.noreply.github.com>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,26 @@
-import { useColorMode, IconButton, Container } from '@chakra-ui/react';
+import { useColorMode, IconButton, Container, useColorModeValue } from '@chakra-ui/react';
 import { SunIcon, MoonIcon } from '@chakra-ui/icons';
 
 export function Header() {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { toggleColorMode } = useColorMode();
+  const { color, icon } = useColorModeValue({ color: 'gray.800', icon: <MoonIcon /> }, { color: 'yellow.500', icon: <SunIcon /> })
 
   return (
     <Container
       maxW="160ch"
-      justifyContent={'end'}
-      alignItems={'end'}
-      display={'flex'}
+      justifyContent="end"
+      alignItems="end"
+      display="flex"
     >
-      {/* IconButton */}
-      {colorMode === 'light' ? (
-        <IconButton
-          aria-label="Switcher"
-          justifyItems={'end'}
-          my="20px"
-          m="8"
-          isRound
-          color="gray.800"
-          icon={<MoonIcon />}
-          variant="outline"
-          onClick={toggleColorMode}
-        />
-      ) : (
-        <IconButton
-          aria-label="Switcher"
-          my="20px"
-          m="8"
-          isRound
-          color="yellow.500"
-          icon={<SunIcon />}
-          variant="outline"
-          onClick={toggleColorMode}
-        />
-      )}
+      <IconButton
+        aria-label="Switcher"
+        m="8"
+        isRound
+        color={color}
+        icon={icon}
+        variant="outline"
+        onClick={toggleColorMode}
+      />
     </Container>
   );
 }
