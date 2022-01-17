0456118c28cc65b491613275ca21fd8045aa3850
victordantasdev <victordantasdeveloper@gmail.com>
2022-01-17 09:19:07 -0300

    refactor: suggestions for improvements to the cli code done

--- apps/cli/bin/cli.js
+++ apps/cli/bin/cli.js
@@ -4,8 +4,10 @@ const open = require('open');
 const { showHelp, showVersion, showErrorMessage } = require('../index');
 
 async function main() {
+  const validFlags = ['-v', '--version', '-h', ' --help'];
   const message = process.argv[2];
 
+<<<<<<< HEAD
   const validOptions = ['-v', '--version', '-h', ' --help'];
 
   if (message.charAt(0) === '-' && !validOptions.includes(message)) {
@@ -28,6 +30,25 @@ async function main() {
         `https://twitter.com/intent/tweet?text=${message}%0Acc%20%40sseraphini`,
       );
       break;
+=======
+  if (
+    message === undefined ||
+    message.toLowerCase() === '-h' ||
+    message.toLowerCase() === '--help'
+  ) {
+    showHelp();
+  } else if (
+    message.toLowerCase() === '-v' ||
+    message.toLowerCase() === '--version'
+  ) {
+    showVersion();
+  } else if (message.charAt(0) === '-' && !validFlags.includes(message)) {
+    showErrorMsg(message);
+  } else {
+    return await open(
+      `https://twitter.com/intent/tweet?text=${message}%0Acc%20%40sseraphini`,
+    );
+>>>>>>> 84b0960 (refactor: suggestions for improvements to the cli code done)
   }
 }
 
