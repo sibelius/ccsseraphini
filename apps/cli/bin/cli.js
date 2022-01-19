84729baeec13ea4128bb7bbdc95bd4043600d211
victordantasdev <victordantasdeveloper@gmail.com>
2022-01-19 08:29:50 -0300

    fix: some code bugs

--- apps/cli/bin/cli.js
+++ apps/cli/bin/cli.js
@@ -1,18 +1,11 @@
 #!/usr/bin/env node
 
 const open = require('open');
-<<<<<<< HEAD
 const { showHelp, showVersion, showErrorMessage } = require('../index');
-=======
-const { showHelp, showVersion, showErrorMsg } = require('../index');
->>>>>>> origin
 
 async function main() {
   const message = process.argv[2];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   const validOptions = ['-v', '--version', '-h', ' --help'];
 
   if (message.charAt(0) === '-' && !validOptions.includes(message)) {
@@ -35,51 +28,7 @@ async function main() {
         `https://twitter.com/intent/tweet?text=${message}%0Acc%20%40sseraphini`,
       );
       break;
-=======
-=======
-  const validOptions = ['-v', '--version', '-h', ' --help'];
-  const options = {
-    help: ['-h', '--help', undefined],
-    version: ['-v', '--version'],
-  }
-
->>>>>>> 0b21f2f (refactor: updating general code parts to improve consistency)
-  if (
-    options.help.includes(message?.toLowerCase())
-  ) {
-    showHelp();
-  } else if (
-    options.version.includes(message?.toLowerCase())
-  ) {
-    showVersion();
-  } else if (message.charAt(0) === '-' && !validOptions.includes(message)) {
-    showErrorMessage(message);
-=======
-  if (message === undefined || message === '-h' || message === '--help') {
-    showHelp();
-  } else if (message === '-v' || message === '--version') {
-    showVersion();
-  } else if (
-    (message.charAt(0) === '-' && message !== '-h') ||
-    (message.charAt(0) === '-' && message !== '--help') ||
-    (message.charAt(0) === '-' && message !== '-v') ||
-    (message.charAt(0) === '-' && message !== '--version')
-  ) {
-    showErrorMsg(message);
->>>>>>> origin
-  } else {
-    return await open(
-      `https://twitter.com/intent/tweet?text=${message}%0Acc%20%40sseraphini`,
-    );
-<<<<<<< HEAD
->>>>>>> 84b0960 (refactor: suggestions for improvements to the cli code done)
   }
 }
 
 main();
-=======
-  }
-}
-
-main();
->>>>>>> origin
