6005191233bf07afd0f2c8fcb1c888e399954608
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-01-21 02:47:47 +1300

    feat(cli/test): add jest and unit tests to apps/cli

--- apps/cli/.eslintrc.js
+++ apps/cli/.eslintrc.js
@@ -3,23 +3,11 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  rules: {
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        ignoreRestSiblings: true,
-      },
-    ],
-    '@typescript-eslint/ban-ts-comment': 1,
-    '@typescript-eslint/no-var-requires': 1,
-  },
+  rules: {},
 };
