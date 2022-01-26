b6b030eff72a6443e7e34ed56b5cb15ce640239c
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2022-01-25 22:23:38 -0300

    fix(ci): fix tests

--- apps/cli/jest.config.js
+++ apps/cli/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(js|ts|tsx)?$': 'babel-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
