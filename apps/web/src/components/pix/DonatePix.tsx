5b10af2fa7a9ed58c6b4ac1dc703f49e5c358279
Fernando Silva <fernando457829@gmail.com>
2022-01-26 11:54:26 -0300

    fix: donate pix button

--- apps/web/src/components/pix/DonatePix.tsx
+++ apps/web/src/components/pix/DonatePix.tsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
 import { useState } from 'react';
 import { PixModal } from './PixModal';
 import { PixLogo } from './PixLogo';
@@ -16,25 +16,18 @@ export const DonatePix = () => {
 
   return (
     <>
-      <Stack
-        spacing={2}
-        direction={{ base: 'row', md: 'column' }}
-        mt={{ base: '10px', md: '0' }}
-        justify="center"
+      <Button
+        backgroundColor="white"
+        borderWidth="1px"
+        borderColor="gray.500"
+        borderRadius="lg"
+        leftIcon={<PixLogo width={20} height={20} />}
+        mt="10px"
+        size="sm"
+        onClick={openModal}
       >
-        <Button
-          backgroundColor="white"
-          borderWidth="1px"
-          borderColor="gray.500"
-          borderRadius="lg"
-          leftIcon={<PixLogo width={20} height={20} />}
-          mt={{ base: '0', md: '10px' }}
-          size="sm"
-          onClick={openModal}
-        >
-          Donate PIX
-        </Button>
-      </Stack>
+        Donate PIX
+      </Button>
       <PixModal isOpen={isModalOpen} closeModal={closeModal} />
     </>
   );
