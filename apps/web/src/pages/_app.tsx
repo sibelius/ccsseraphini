f82c5528ec7ea07f5eb22074dbef9c1289cb4006
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2022-02-03 20:52:07 +0000

    style: hook theme into the app

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import theme from '../styles/theme';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
