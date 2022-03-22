97e1074d8bdeb3d91682fe7a509ced9611e17deb
Pablo Jonatan <this.pablo@gmail.com>
2022-03-21 22:33:36 -0300

    feat(score): added score page

---
+++ apps/web/src/components/score/Score.tsx
@@ -0,0 +1,66 @@
+import { Heading, Flex, VStack } from '@chakra-ui/react';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { UserScore } from 'types/Score';
+
+export const Score = () => {
+  const { data: session } = useSession();
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [score, setScore] = useState<UserScore>({
+    retweet_count: 0,
+    reply_count: 0,
+    like_count: 0,
+    quote_count: 0,
+    total: 0,
+  });
+
+  useEffect(() => {
+    if (session) {
+      const updateScore = async (session: any) => {
+        const body = new URLSearchParams({
+          providerAccountId: session?.id,
+          access_token: session?.access_token as string,
+          username: 'pjonatansr', //TO-DO GET USERNAME FROM PROFILE
+        }).toString();
+        setLoading(true);
+
+        const { userScore } = await fetch(`/api/userScore`, {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          method: 'POST',
+          body,
+        })
+          .then(async (response) => await response.json())
+          .catch((error: any) => {
+            setErrors(error);
+          });
+
+        setScore(userScore);
+        setLoading(false);
+      };
+
+      updateScore(session);
+    }
+  }, [session]);
+
+  if (!!errors) {
+    return <Heading>Oops! Something is wrong!</Heading>;
+  }
+
+  if (!!session && !loading) {
+    return (
+      <Flex>
+        <VStack>
+          <Heading>{session?.user?.name || 'Guest'}, your score is</Heading>
+          <Heading>
+            Replies: {score.reply_count} Likes: {score.like_count} Retweets:{' '}
+            {score.retweet_count} Quotes: {score.quote_count}
+          </Heading>
+          <Heading>Total: {score.total}</Heading>
+        </VStack>
+      </Flex>
+    );
+  }
+
+  return <Heading> Loading...</Heading>;
+};
--- apps/web/src/pages/api/userScore.ts
+++ apps/web/src/pages/api/userScore.ts
@@ -1,35 +1,15 @@
-import { AccountProfile } from 'types/Score';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { UserScore, Tweet } from 'types/Score';
+import { config } from '../../config';
 import { userTweets } from '../../modules/twitter/twitterFollowersGet';
 
-export const userScore = async ({
-  account: { providerAccountId, access_token },
-  profile: {
-    data: { username },
-  },
-}: AccountProfile) => {
-  if (!access_token) {
-    return 0;
-  }
-
-  const result = await userTweets(providerAccountId, access_token);
-
-  if (!result.data) {
-    return 0;
-  }
-
-  const { data } = result;
-
-  const currentUser = username;
-  interface PublicMetrics {
-    public_metrics: {
-      retweet_count: number;
-      reply_count: number;
-      like_count: number;
-      quote_count: number;
-    };
-  }
-
-  const scoreDetail = {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { body } = req;
+  const { providerAccountId, access_token, username } = body;
+  const emptyScore: UserScore = {
     retweet_count: 0,
     reply_count: 0,
     like_count: 0,
@@ -37,33 +17,71 @@ export const userScore = async ({
     total: 0,
   };
 
-  const score = data
-    .filter((tweet: { text: string; in_reply_to_user_id: string }) => {
-      const { text, in_reply_to_user_id } = tweet;
+  if (!access_token) {
+    return res.status(401).json({
+      scoreDetail: emptyScore,
+    });
+  }
 
-      const isMentioned = text.match(/cc(:?\s*)@sseraphini/gi);
-      const isReplied = text.match(
-        new RegExp(`@${currentUser} @sseraphini`, 'gi'),
+  if (!config.TWITTER_PROFILE_ID || !config.TWITTER_PROFILE_USER) {
+    return res.status(500).json({
+      scoreDetail: emptyScore,
+    });
+  }
+
+  const result = await userTweets(
+    providerAccountId as string,
+    access_token as string,
+  );
+  if (!result.data) {
+    return res.status(204).json({
+      scoreDetail: emptyScore,
+    });
+  }
+
+  const { data } = result;
+  const elegibleTweets: Tweet[] = data.filter(
+    ({ text, in_reply_to_user_id }: Tweet) => {
+      const isReplyingProfileId =
+        in_reply_to_user_id === config.TWITTER_PROFILE_ID;
+      const isProfileUserMentioned = text.match(
+        new RegExp(`cc(:?\s*)@${config.TWITTER_PROFILE_USER}`, 'gi'),
       );
-      const isReplyingToUser = in_reply_to_user_id === '2313873457';
-
-      return isMentioned || isReplied || isReplyingToUser;
-    })
-    .reduce((accumulator: number, current: PublicMetrics): number => {
-      const { retweet_count, reply_count, like_count, quote_count } =
-        current.public_metrics;
-
-      scoreDetail.retweet_count += retweet_count;
-      scoreDetail.reply_count += reply_count;
-      scoreDetail.like_count += like_count;
-      scoreDetail.quote_count += quote_count;
-
-      return (
-        accumulator + retweet_count + reply_count + like_count + quote_count
+      const isUserReplied = text.match(
+        new RegExp(`@${username} @${config.TWITTER_PROFILE_USER}`, 'gi'),
       );
-    }, 0);
 
-  scoreDetail.total = score;
+      return isProfileUserMentioned || isReplyingProfileId || isUserReplied;
+    },
+  );
 
-  return scoreDetail;
-};
+  const userScore: UserScore = elegibleTweets.reduce(
+    (
+      accumulator: UserScore,
+      {
+        public_metrics: { retweet_count, reply_count, like_count, quote_count },
+      }: Tweet,
+    ): UserScore => {
+      const retweets = retweet_count + accumulator.retweet_count;
+      const replies = reply_count + accumulator.reply_count;
+      const likes = like_count + accumulator.like_count;
+      const quotes = quote_count + accumulator.quote_count;
+      const total = accumulator.total + retweets + replies + likes + quotes;
+
+      const currentUserScore = {
+        retweet_count: retweets,
+        reply_count: replies,
+        like_count: likes,
+        quote_count: quotes,
+        total,
+      };
+
+      return currentUserScore;
+    },
+    emptyScore,
+  );
+
+  return res.status(200).json({
+    userScore,
+  });
+}
