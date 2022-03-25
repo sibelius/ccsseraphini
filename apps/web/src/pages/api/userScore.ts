2573704997ba39a33f44d86c18222986e1d0bf32
Pablo Jonatan <this.pablo@gmail.com>
2022-03-24 21:35:45 -0300

    feat(score): use twitter bearer token

--- apps/web/src/pages/api/userScore.ts
+++ apps/web/src/pages/api/userScore.ts
@@ -8,7 +8,7 @@ export default async function userScoreHandler(
   res: NextApiResponse,
 ) {
   const { body } = req;
-  const { providerAccountId, access_token, username } = body;
+  const { providerAccountId, username } = body;
   const emptyScore: UserScore = {
     retweet_count: 0,
     reply_count: 0,
@@ -17,6 +17,7 @@ export default async function userScoreHandler(
     total: 0,
   };
 
+  const access_token = config.TWITTER_BEARER_TOKEN;
   if (!access_token) {
     return res.status(401).json({
       message: 'Authorization required',
