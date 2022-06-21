e40cc55dd4571abf04ac2226d96e755375787647
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-06-21 01:20:54 -0300

    Feat: Added speakers section

---
+++ apps/web/src/pages/sibs-day/speaker/speaker-list.ts
@@ -0,0 +1,22 @@
+import { SpeakerProps } from './index';
+
+const speakers: Array<SpeakerProps> = [
+  {
+    direction: 'row-reverse',
+    image:
+      'https://pbs.twimg.com/profile_images/1494329046678659072/RprvW5s4_400x400.jpg',
+    name: 'sseraphini',
+    handler: 'sseraphini',
+    talk: 'React Hooks: Como usarlas en una aplicación de React',
+  },
+  {
+    direction: 'row',
+    image:
+      'https://pbs.twimg.com/profile_images/1494329046678659072/RprvW5s4_400x400.jpg',
+    name: 'sseraphini',
+    handler: 'sseraphini',
+    talk: 'React Hooks: Una aplicación de React',
+  },
+];
+
+export default speakers;
