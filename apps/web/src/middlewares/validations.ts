72f6fe242f3660ccacb8160c95956bbfa459f5b0
Pablo Jonatan <this.pablo@gmail.com>
2022-07-13 07:59:36 -0300

    test(validations): added tests to validations

--- apps/web/src/middlewares/validations.ts
+++ apps/web/src/middlewares/validations.ts
@@ -1,29 +1,34 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 import { config } from '../config';
 
-// TODO create tests for this function
 const withValidation =
   (fn: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       // Adding new security layer, make hard to hackers using the browser for DDoS atack
-      if (req.method !== 'POST') res.status(405).send('method not allowed');
+      if (req.method !== 'POST') {
+        return res.status(405).send('method not allowed');
+      }
 
       if (!config.CRONJOB_HEADER_KEY) {
-        res.status(500).json({ error: 'env CRONJOB_HEADER_KEY not set' });
+        return res
+          .status(500)
+          .json({ error: 'env CRONJOB_HEADER_KEY not set' });
       }
 
       if (!config.CRONJOB_HEADER_VALUE)
-        res.status(500).json({ error: 'env CRONJOB_HEADER_VALUE not set' });
+        return res
+          .status(500)
+          .json({ error: 'env CRONJOB_HEADER_VALUE not set' });
 
       if (!req.headers[config.CRONJOB_HEADER_KEY])
-        res
+        return res
           .status(400)
           .send('CronjobOrg should provide secretkey in the header');
 
       if (
         req.headers[config.CRONJOB_HEADER_KEY] !== config.CRONJOB_HEADER_VALUE
       ) {
-        res
+        return res
           .status(401)
           .send(
             'Ops! Wrong header value, check the env variable or cronjob.org account',
