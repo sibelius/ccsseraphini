a5d489da17adddd05ef2ba3467fa0090a0639a68
Pablo Jonatan <this.pablo@gmail.com>
2022-07-31 10:46:14 -0300

    chore: improve status on mobile (#906)

--- apps/web/src/pages/status.tsx
+++ apps/web/src/pages/status.tsx
@@ -15,53 +15,52 @@ import { MdCheckCircle } from 'react-icons/md';
 
 const Status: NextPage = (): JSX.Element => {
   return (
-    <div>
+    <Flex
+      flex="1"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      h="100vh"
+      bgColor={bgPalette.base}
+      textColor={txtPalette.base}
+      p="1"
+    >
+      <Heading textAlign="center">
+        cc
+        <Link href="https://twitter.com/sseraphini">
+          &nbsp;@sseraphini&nbsp;
+        </Link>
+        is always on
+      </Heading>
+      <Text textAlign="center">even when stackoverflow is down</Text>
       <Flex
-        flex={1}
+        mt="10"
+        flexDirection="row"
         alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-        h="100vh"
-        bgColor={bgPalette.base}
-        textColor={txtPalette.base}
+        justifyContent="space-around"
+        flexWrap="wrap"
       >
-        <Heading>
-          cc
-          <Link href="https://twitter.com/sseraphini">
-            &nbsp;@sseraphini&nbsp;
-          </Link>
-          is always on
-        </Heading>
-        <Text>even when stackoverflow is down</Text>
+        <RandomProvider>
+          <ParticleSibAvatar />
+        </RandomProvider>
 
-        <Flex
-          mt="10"
-          flexDirection={'row'}
-          alignItems="center"
-          justifyContent="space-around"
-        >
-          <RandomProvider>
-            <ParticleSibAvatar />
-          </RandomProvider>
-
-          <List spacing={3} ml="10">
-            {[
-              'sseraphini.cc is always on',
-              'cc @sseraphini bot is always on',
-              '@sseramemes is always on',
-              'chrome extension is always on',
-              'cli tool is always on',
-              'vscode extension is always on',
-            ].map((text, index) => (
-              <ListItem key={index}>
-                <ListIcon color="green.500" as={MdCheckCircle} />
-                {text}
-              </ListItem>
-            ))}
-          </List>
-        </Flex>
+        <List spacing="3" ml="10">
+          {[
+            'sseraphini.cc is always on',
+            'cc @sseraphini bot is always on',
+            '@sseramemes is always on',
+            'chrome extension is always on',
+            'cli tool is always on',
+            'vscode extension is always on',
+          ].map((text, index) => (
+            <ListItem key={index}>
+              <ListIcon color="green.500" as={MdCheckCircle} />
+              {text}
+            </ListItem>
+          ))}
+        </List>
       </Flex>
-    </div>
+    </Flex>
   );
 };
 
