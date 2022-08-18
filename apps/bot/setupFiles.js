c69b95f2af046675f63f16d8525d3105018c5528
Pablo Jonatan <this.pablo@gmail.com>
2022-08-17 23:32:26 -0300

    fix(setupFiles): added enviroment vars to process.env to fix tests

--- apps/bot/setupFiles.js
+++ apps/bot/setupFiles.js
@@ -1,2 +1,18 @@
 const fetchMock = require('jest-fetch-mock');
 fetchMock.enableFetchMocks();
+
+[
+  'DISCORD_TOKEN',
+  'DISCORD_CLIENT_ID',
+  'DISCORD_CLIENT_SECRET',
+  'DISCORD_BOT_TOKEN',
+  'DISCORD_BOT_CHANNEL_ID',
+  'TWITTER_BEARER_TOKEN',
+  'TWITTER_ACCESS_TOKEN',
+  'TWITTER_ACCESS_TOKEN_SECRET',
+  'TWITTER_API_KEY',
+  'TWITTER_API_KEY_SECRET',
+  'MONGO_URI',
+].forEach((key) => {
+  process.env[key] = key;
+});
