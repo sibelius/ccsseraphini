d66b96fe8a42aaa6d29622a55fe8c744cde49bb6
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2022-10-27 16:57:33 -0300

    feat: adjusted opacity when image is small (#1158)

--- apps/sseramemes/src/image-scripts/addLogoToImage.ts
+++ apps/sseramemes/src/image-scripts/addLogoToImage.ts
@@ -1,6 +1,6 @@
 import Jimp from 'jimp';
 import * as path from 'path';
-import { resizeImage } from './index';
+import { resizeImage, TWITTER_IMAGE_SIZE } from './index';
 
 export const addLogoToImage = async (buffer: Buffer): Promise<Buffer> => {
   const logo: Jimp = await Jimp.read(
@@ -8,6 +8,12 @@ export const addLogoToImage = async (buffer: Buffer): Promise<Buffer> => {
   );
   logo.resize(40, 40);
   const meme: Jimp = await Jimp.read(buffer);
+  if (
+    meme.getHeight() < TWITTER_IMAGE_SIZE &&
+    meme.getWidth() < TWITTER_IMAGE_SIZE
+  ) {
+    logo.opacity(0.5);
+  }
   meme.composite(logo, meme.getWidth() - 50, meme.getHeight() - 50);
   const memeBuffer = await meme.getBufferAsync(meme.getMIME());
   return await resizeImage(memeBuffer);
