531474308af785890ef846f30e085f49ec89c873
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-17 22:37:34 -0300

    tests: added tests to add to text file

---
+++ apps/sseramemes/scripts/testAddTextToImage.ts
@@ -0,0 +1,26 @@
+import fetch from 'node-fetch';
+import * as fs from 'fs';
+import { addLogoToImage, addTextToImage } from '../src/image-scripts';
+import { Message } from 'discord.js';
+
+const testMeme = async () => {
+  const image = await fetch(
+    'https://pbs.twimg.com/media/FSbQR8oXsAIKkQz?format=png&name=900x900',
+  );
+
+  const buffer = Buffer.from(await image.arrayBuffer());
+  const bufferWithText = await addTextToImage(
+    {
+      content:
+        ';meme text="eai jeff" position="bottom-center" color="medium-white"',
+    } as Message,
+    buffer,
+  );
+  const bufferWithWatermark = await addLogoToImage(bufferWithText);
+  /**
+   * Save bufferWithWatermark to file.
+   */
+  await fs.promises.writeFile('test.png', bufferWithWatermark);
+};
+
+testMeme().then();
