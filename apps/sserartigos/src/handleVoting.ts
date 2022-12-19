4cb2f4b8f15e0cab2708c867ce163e1659a95143
Marcus <marcus.xavier@aluno.ufop.edu.br>
2022-12-18 21:38:12 -0300

    feat(sserartigos): creating function to calculate poll score

--- apps/sserartigos/src/handleVoting.ts
+++ apps/sserartigos/src/handleVoting.ts
@@ -9,3 +9,15 @@ export const addVoting = (message: Message) => {
   message.react('💯');
   message.react('👎');
 };
+const isVotingDone = (message: Message | PartialMessage): boolean => {
+  return calculateScore(message.reactions.cache) >= MIN_POINTS_TO_PUSH;
+};
+
+export const calculateScore = (
+  reactions: Collection<string, MessageReaction>,
+): number => {
+  return reactions.reduce((acc, value, key) => {
+    const points = EMOJIS_POINTS[key] ?? 0;
+    return (value.count - 1) * points + acc;
+  }, 0);
+};
---
+++ apps/sserartigos/src/score.ts
@@ -0,0 +1,6 @@
+export const MIN_POINTS_TO_PUSH = 1;
+
+export const EMOJIS_POINTS = {
+  '💯': 1,
+  '👎': -3,
+};
