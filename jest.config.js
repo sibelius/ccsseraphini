4987f44c635463db98ce0a53f6716061a4687fa8
Pablo Jonatan <this.pablo@gmail.com>
2022-12-23 10:56:05 -0300

    test: add strict query to mongoose

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     '<rootDir>/apps/cli/jest.config.js',
     '<rootDir>/apps/web/jest.config.js',
     '<rootDir>/apps/sseramemes/jest.config.js',
+    '<rootDir>/apps/ranking/jest.config.js',
+    '<rootDir>/apps/bot/jest.config.js',
   ],
   transformIgnorePatterns: ['node_modules/(?!use-debounce|uuid)'],
   coverageProvider: 'v8',
