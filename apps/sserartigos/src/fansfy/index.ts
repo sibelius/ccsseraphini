b9cecbf2ff82109c85941c06dfdd4448924cc24f
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-01-04 23:36:51 -0300

    feat(sserartigos): creating fansfy module to post articles on it

---
+++ apps/sserartigos/src/fansfy/index.ts
@@ -0,0 +1,19 @@
+import { postArticle } from './postArticle';
+
+export const ENDPOINT_URL = 'https://api.fansfy.io/v1/publish';
+
+export const postAllArticles = async (links: string[]) => {
+  const promisses = links.map((link) => {
+    return postArticle(link);
+  });
+
+  await Promise.all(promisses).then((responses) => {
+    responses.forEach(verifyIfHasError);
+  });
+
+  return;
+};
+
+function verifyIfHasError(res: Response) {
+  if (!res.ok) throw new Error(`response from API was: ${res.status}`);
+}
