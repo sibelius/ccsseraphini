bd03069e77181b60aeb3767d3be1feb8a5aaa6c0
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-01-05 22:59:31 -0300

    feat(sserartigos): improving link validation
    Now, it will discard links from twitter, youtube and google.

--- apps/sserartigos/src/common/utils/getArticles.ts
+++ apps/sserartigos/src/common/utils/getArticles.ts
@@ -3,3 +3,19 @@ export const getArticles = (messageContent: string): string[] => {
   const links = messageContent.match(urlRegex) ?? [];
   return filterBlockedDomains(links);
 };
+/**
+ * Websites whose the bot should ignore
+ * */
+const BLOCKED_DOMAINS = ['twitter.com', 'google.com', 'youtube.com'];
+
+function filterBlockedDomains(links: string[]) {
+  return links.filter(isNotBlocked);
+}
+
+function isNotBlocked(link: string) {
+  const hasBlockedDomain = BLOCKED_DOMAINS.reduce(
+    (acc, domain) => acc || link.includes(domain),
+    false,
+  );
+  return !hasBlockedDomain;
+}
