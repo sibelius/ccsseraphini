3f0642ab23649dc0e342acdc06336d8380701ac1
Pedro Arantes <arantespp@gmail.com>
2023-01-17 22:19:02 -0300

    update score.ts for community safe (#1511)

--- apps/sseramemes/src/score.ts
+++ apps/sseramemes/src/score.ts
@@ -1,8 +1,8 @@
-export const MIN_POINTS_TO_TWEET_MEME = 1;
+export const MIN_POINTS_TO_TWEET_MEME = 2;
 
 export const EMOJIS_POINTS = {
   '💯': 1,
-  '👎': -3,
+  '👎': -5,
 };
 
 export const RETWEET_MEME_TIMEOUT = 1000 * 60 * 60; // 1 hour
