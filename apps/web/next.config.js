b6ad2dfc63ca25fede52eac8ac3320c4c8438151
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2023-03-23 21:55:15 -0300

    fix(overrated): eslint and ts is overrated (#1771)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -16,6 +16,16 @@ const nextConfig = {
   images: {
     domains: ['pbs.twimg.com', 'cdn.discordapp.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 const plugins = [
