fbb6e32c767f9b9c7718fbc2d8213b0a2f3e7669
Marcus <marcus.xavier@aluno.ufop.edu.br>
2023-05-08 17:45:30 -0300

    feat: add automatic link emoji on every message with an article

--- apps/sserartigos/src/pollHandler.ts
+++ apps/sserartigos/src/pollHandler.ts
@@ -1,3 +1,4 @@
+import { getArticles } from './common/utils/getArticles';
 import { checkVotingAbility } from './common/utils/utils';
 import { DiscordMessage } from './types';
 
@@ -5,6 +6,7 @@ import { DiscordMessage } from './types';
  * Messages that already have a poll.
  */
 const messagesWithPoll = [];
+const messagesWithLinkEmoji = [];
 
 export const createPoll = (message: DiscordMessage) => {
   if (messagesWithPoll.includes(message.id) || !checkVotingAbility(message))
@@ -13,4 +15,16 @@ export const createPoll = (message: DiscordMessage) => {
   messagesWithPoll.push(message.id);
   message.react('💯');
   message.react('👎');
+};
+
+export const addLinkEmoji = (message: DiscordMessage) => {
+  const canAddEmoji =
+    getArticles(message.content).length >= 1 && message.attachments.size === 0;
+
+  if (messagesWithLinkEmoji.includes(message.id) || !canAddEmoji) {
+    return;
+  }
+
+  messagesWithLinkEmoji.push(message.id);
+  message.react('🔗');
 };
